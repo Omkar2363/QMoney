d1701eb0beb1bf26c934896da2533e0d52f16082
Omkar2363 <Om.236300@gmail.com>
2024-07-27 16:00:11 +0530

    Add QMoney Project to Git Repository

---
+++ tingo_curl.sh
@@ -0,0 +1,3 @@
+curl "https://api.tiingo.com/tiingo/daily/aapl/prices?startDate=2019-01-02&token=d4ee45efc729a932be1fb37b9f7688ea2e987e2a"
+curl "https://api.tiingo.com/tiingo/daily/googl/prices?startDate=2019-01-02&token=d4ee45efc729a932be1fb37b9f7688ea2e987e2a" 
+curl "https://api.tiingo.com/tiingo/daily/msft/prices?startDate=2019-01-02&token=d4ee45efc729a932be1fb37b9f7688ea2e987e2a"
